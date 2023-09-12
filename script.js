cfe86e81a181db2fbe8ebde67a14b9ba43230587
Tyler Hudson <tylerhudson96@gmail.com>
2023-09-11 22:09:14 -0400

    Added variables for DOM elements for future targeting

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const passwordInput = document.getElementById("user-password");
+const passwordConfirmationInput = document.getElementById("confirm-password");
 
+const passwordErrorText = document.getElementById("password-error");
+const passwordConfirmationErrorText = document.getElementById(
+  "password-confirmation-error"
+);
